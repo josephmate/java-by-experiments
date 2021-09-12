f22153b9538306ef553c9483deb41c32f91d6e03
Joseph <mate.joseph@gmail.com>
2021-09-12 18:24:10 -0400

    Created synchronized deadlock

--- deadlock_synchronized/Main.java
+++ deadlock_synchronized/Main.java
@@ -1,8 +1,40 @@
 import java.util.*;
+import java.util.concurrent.CyclicBarrier;
 
 public final class Main {
   
   public static void main(String [] args) throws Exception {
+    final Object lockA = new Object();
+    final Object lockB = new Object();
+    final CyclicBarrier barrier = new CyclicBarrier(2);
+
+    Thread thread1 = new Thread(() -> {
+      synchronized(lockA) {
+        try {
+          barrier.await();
+        } catch (Exception e) { // don't do this in production code :)
+        }
+        synchronized(lockB) {
+          System.out.println("Got A then B");
+        }
+      }
+    });
+    thread1.start();
+    Thread thread2 = new Thread(() -> {
+      synchronized(lockB) {
+        try {
+          barrier.await();
+        } catch (Exception e) { // don't do this in production code :)
+        }
+        synchronized(lockA) {
+          System.out.println("Got B then A");
+        }
+      }
+    });
+    thread2.start();
+    
+    thread1.join();
+    thread2.join();
   }
   
 }
--- deadlock_synchronized/README.md
+++ deadlock_synchronized/README.md
@@ -8,6 +8,22 @@ What happens when you get a deadlock with synchronized in java?
 <details>
 <summary>Click to reveal the results of my experiment.</summary>
 
-TODO
+To create a deadlock, we need to create a cycle in the wait for resource graph.
+The easier way to do that is have one thread grab synchronized lock A then B.
+The other thread need to grab B then A.
+
+However, there's a catch!
+That won't reliably reproduce the problem 100% of the time.
+Sometimes, the thread will storm through and grab A then B before the other
+thread even starts to grab B.
+As a result, we introduce a barrier that will wait until thread 1 grab lock a and thread 2 grabs lock b.
+
+```
+./run.sh
+<process never exists>
+```
+As expected, the process never ends because thread 1 and 2 are deadlocked, wait for the other to give up their resource.
+Java does not detect this deadlock for you unless you ask it to.
+
 </details>
 
