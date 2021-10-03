c47afd3456ba3e988b7cae6455be90c5c947637f
Joseph <mate.joseph@gmail.com>
2021-10-03 16:06:46 -0400

    minimized lines of code needed to reproduce issue

--- experimental/scheduled_thread_pool_executor_100_cpu/Main.java
+++ experimental/scheduled_thread_pool_executor_100_cpu/Main.java
@@ -1,9 +1,6 @@
-import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.locks.ReentrantLock;
 
 public final class Main {
   
