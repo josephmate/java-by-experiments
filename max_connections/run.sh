f6dba919af4f84e44c9a106503b06bf98bb3228f
Joseph Mate <mate.joseph@gmail.com>
2022-04-10 12:53:42 -0400

    Added flame graphs to run

--- max_connections/run.sh
+++ max_connections/run.sh
@@ -7,6 +7,16 @@ javac src/Main.java
 sar -o out.$NUM_CONNECTIONS.sar -A 1 3600 2>&1 > /dev/null  &
 SAR_PID=$!
 
-java -cp src/ Main $NUM_CONNECTIONS | tee out.$NUM_CONNECTIONS.txt
+echo running:
+echo java \
+	-agentpath:/home/joseph/lib/async-profiler-2.7-linux-x64/build/libasyncProfiler.so=start,collapsed,event=wall,file=collapsed.wall.$NUM_CONNECTIONS.txt \
+	-cp src/ \
+	Main $NUM_CONNECTIONS
+
+java \
+	-agentpath:/home/joseph/lib/async-profiler-2.7-linux-x64/build/libasyncProfiler.so=start,collapsed,event=wall,file=collapsed.wall.$NUM_CONNECTIONS.txt \
+	-cp src/ \
+	Main $NUM_CONNECTIONS \
+	| tee out.$NUM_CONNECTIONS.txt
 
 kill $SAR_PID
