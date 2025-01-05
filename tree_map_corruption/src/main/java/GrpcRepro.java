9df8b40f513ae9cbd6095fab3c309c730b5b61d1
Joseph Mate <mate.joseph@gmail.com>
2025-01-04 20:41:41 -0500

    wait on the futures

--- tree_map_corruption/src/main/java/GrpcRepro.java
+++ tree_map_corruption/src/main/java/GrpcRepro.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 
+import com.google.common.util.concurrent.ListenableFuture;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.Server;
@@ -32,13 +35,18 @@ public class GrpcRepro {
             .build();
         final ReceiptProcessorServiceGrpc.ReceiptProcessorServiceFutureStub futureStub = ReceiptProcessorServiceGrpc.newFutureStub(channel);
 
+        List<ListenableFuture<?>> futures = new ArrayList<>();
         Random random = new Random();
         for (int i = 0; i < 100_000; i++) {
             ReceiptProcessorServiceOuterClass.AddReceiptRequest request = ReceiptProcessorServiceOuterClass.AddReceiptRequest.newBuilder()
                 .setTimestamp(random.nextInt(10_000))
                 .setTotalPrice(random.nextInt(10_000))
                 .build();
-            futureStub.addReceipt(request);
+            futures.add(futureStub.addReceipt(request));
+        }
+
+        for (ListenableFuture<?> future : futures) {
+            future.get();
         }
 
         server.shutdown();
--- tree_map_corruption/src/main/java/GrpcThrowNPE.java
+++ tree_map_corruption/src/main/java/GrpcThrowNPE.java
@@ -1,19 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 
+import com.google.common.util.concurrent.ListenableFuture;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 
-/**
- * Reproduces the corrupted TreeMap issues through Grpc rather than directly through threads.
- *
- * Demonstrates that the simplified 2 thread reproduce code isn't that unrealistic. Exceptionsm
- * get accidentally swallowed all the time especially if there are RuntimeExceptions like NPEs.
- */
 public class GrpcThrowNPE {
     public static void main(String[] args) throws Exception {
         final Server server = ServerBuilder.forPort(0)
@@ -32,13 +29,18 @@ public class GrpcThrowNPE {
             .build();
         final ReceiptProcessorServiceGrpc.ReceiptProcessorServiceFutureStub futureStub = ReceiptProcessorServiceGrpc.newFutureStub(channel);
 
+        List<ListenableFuture<?>> futures = new ArrayList<>();
         Random random = new Random();
-        for (int i = 0; i < 1_000; i++) {
+        for (int i = 0; i < 100; i++) {
             ReceiptProcessorServiceOuterClass.AddReceiptRequest request = ReceiptProcessorServiceOuterClass.AddReceiptRequest.newBuilder()
                 .setTimestamp(random.nextInt(10_000))
                 .setTotalPrice(random.nextInt(10_000))
                 .build();
-            futureStub.addReceipt(request);
+            futures.add(futureStub.addReceipt(request));
+        }
+
+        for (ListenableFuture<?> future : futures) {
+            future.get();
         }
 
         server.shutdown();
