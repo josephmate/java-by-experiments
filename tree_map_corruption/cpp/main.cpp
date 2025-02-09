67a7c98bf3e318482daa0e09d07939d7587cfdf5
Joseph Mate <mate.joseph@gmail.com>
2025-02-09 16:41:32 -0500

    Fix comments

--- tree_map_corruption/cpp/main.cpp
+++ tree_map_corruption/cpp/main.cpp
@@ -9,15 +9,16 @@ int main(int argc, char* argv[]) {
     int numUpdates = (argc >= 3) ? std::stoi(argv[2]) : 100;
         int progressRatio = 10;
 
-    // Equivalent of TreeMap in C++ is std::map (sorted by keys)
+    // Equivalent of TreeMap in C++ is std::map
+		// Uses same red black tree data structure
     std::map<int, int> sortedMap;
 
     std::vector<std::thread> threads;
     std::atomic<int> threadIdGenerator(0);
 
     for (int i = 0; i < numThreads; i++) {
-        // copy so that we don't use the latest value of i (5)
         threads.emplace_back([&]() {
+            // copy so that we don't use the latest value of i (5)
             int threadId = threadIdGenerator.fetch_add(1);
             std::random_device rd;
             std::mt19937 gen(rd());
