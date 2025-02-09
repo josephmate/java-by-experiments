1abe214f51a8e4b8d94e0e3c707e0a8dce323796
Joseph Mate <mate.joseph@gmail.com>
2025-02-09 16:40:04 -0500

    Fix formatting

--- tree_map_corruption/cpp/main.cpp
+++ tree_map_corruption/cpp/main.cpp
@@ -7,24 +7,24 @@
 int main(int argc, char* argv[]) {
     int numThreads = (argc >= 2) ? std::stoi(argv[1]) : 5;
     int numUpdates = (argc >= 3) ? std::stoi(argv[2]) : 100;
-		int progressRatio = 10;
+        int progressRatio = 10;
 
     // Equivalent of TreeMap in C++ is std::map (sorted by keys)
     std::map<int, int> sortedMap;
 
     std::vector<std::thread> threads;
-		std::atomic<int> threadIdGenerator(0);
+    std::atomic<int> threadIdGenerator(0);
 
     for (int i = 0; i < numThreads; i++) {
-				// copy so that we don't use the latest value of i (5)
+        // copy so that we don't use the latest value of i (5)
         threads.emplace_back([&]() {
-						int threadId = threadIdGenerator.fetch_add(1);
+            int threadId = threadIdGenerator.fetch_add(1);
             std::random_device rd;
             std::mt19937 gen(rd());
             std::uniform_int_distribution<> dis(0, 999);
-						
-						int progressThreshold = numUpdates / progressRatio;
-						std::cout << "Thread " << threadId << " started.";
+                        
+            int progressThreshold = numUpdates / progressRatio;
+            std::cout << "Thread " << threadId << " started.";
             for (int j = 0; j < numUpdates; j++) {
                 try {
                     int key = dis(gen);
@@ -32,22 +32,22 @@ int main(int argc, char* argv[]) {
 
                     sortedMap[key] = value;
                 } catch (const std::exception& e) {
-										std::cerr << "Caught exception on thread " << threadId << ":" << e.what() << std::endl;
+                    std::cerr << "Caught exception on thread " << threadId << ":" << e.what() << std::endl;
                 }
 
-								if ((j + 1) % progressThreshold == 0) {
-										std::cout << "Thread " << threadId << ": " 
-												<< ((j + 1) * 100 / numUpdates) << "% complete (" 
-												<< (j + 1) << "/" << numUpdates << " updates)\n";
-								}
+                if ((j + 1) % progressThreshold == 0) {
+                        std::cout << "Thread " << threadId << ": " 
+                                << ((j + 1) * 100 / numUpdates) << "% complete (" 
+                                << (j + 1) << "/" << numUpdates << " updates)\n";
+                }
             }
-						std::cout << "Thread " << threadId << " completed.";
+            std::cout << "Thread " << threadId << " completed.";
         });
     }
 
     // Start all threads
     for (auto& thread : threads) {
-					thread.join();
+        thread.join();
     }
 
     return 0;
